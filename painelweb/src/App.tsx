2a07b067918ae7c138f870e5ebd6d7e8521893ff
Israel Silva <israel@medneo.com.br>
2026-06-17 09:20:31 -0300

    fix: adiciona import esquecido do useRef

--- painelweb/src/App.tsx
+++ painelweb/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Chat, Message, PaletteConfig } from "./types";
 import Sidebar from "./components/Sidebar";
 import ActiveChat from "./components/ActiveChat";
